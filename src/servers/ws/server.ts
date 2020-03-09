3bdcd8ecc71b41302d714b44db8ffc238b848369
Gerasim Gerasimov <dialix@yandex.ru>
2020-03-09 22:13:36 +0700

    дело в асинхронных функциях Tagger.getgetDeviceData

--- src/servers/ws/server.ts
+++ src/servers/ws/server.ts
@@ -1,17 +1,20 @@
+
 import WebSocket = require('ws');
 import {IErrorMessage, ErrorMessage, validationJSON} from '../../utils/types'
-
 interface getDeviceDataFunc {(request: Object): any;}
+import Socket from './wsone';
 
 export default class WSServer {
     private https: any;
     private wss: any;
+    private sockets: Set<Socket>;
 
     private proc: getDeviceDataFunc  = undefined;
 
     constructor (https: any, proc: getDeviceDataFunc) {
         this.https = https;
-        this.proc = proc;;
+        this.proc = proc;
+        this.sockets = new Set<Socket>();
         this.init()
     }
 
@@ -22,51 +25,7 @@ export default class WSServer {
 
     private connectionOnWss( ws: WebSocket) {
         console.log('Connection');
-        ws.on('message', this.onMessage.bind(this, ws));
-        ws.on('close', this.onClose.bind(this, ws))
-    }
-
-    private async onMessage(ws: WebSocket, message: any) {
-        var result: any;
-        try {
-            const request = validationJSON(message);
-            result = await this.decodeCommand(request);
-        } catch (e) {
-            result = ErrorMessage(e.message || '');
-        }
-        ws.send(JSON.stringify(result));
-    }
-
-    private onClose(ws: WebSocket){
-        console.log('Connection close');
-    }
-
-    private async decodeCommand(cmd: any): Promise<any | IErrorMessage> {
-        const key = this.getCmdName(cmd);
-        const commands = {
-            'get'    : this.getDeviceTags.bind(this),
-            'default': () => {
-                return ErrorMessage('Unknown command');
-            }
-        }
-        return await (commands[key] || commands['default'])(cmd[key])
-    }
-
-    private getCmdName(cmd: any): string {
-        for (let key in cmd) {
-            return key;
-          }
-        throw new Error ('Invalid request format');
-    }
-
-    private async getDeviceTags (request: any): Promise<any | IErrorMessage> {
-        try {
-            const result = await this.proc(request)
-            return {status:'OK',
-                    time: new Date().toISOString(),
-                    data:result}
-        } catch (e) {
-            return ErrorMessage(e.message || '');
-        }
+        let socket: Socket = new Socket(ws, this.proc);
+        this.sockets.add(socket);
     }
 }
---
+++ src/servers/ws/wsone.ts
@@ -0,0 +1,61 @@
+import WebSocket = require('ws');
+import {IErrorMessage, ErrorMessage, validationJSON} from '../../utils/types'
+
+interface getDeviceDataFunc {(request: Object): any;}
+
+export default class Socket {
+    private ws: WebSocket
+    private proc: getDeviceDataFunc  = undefined;
+    private commands: any;
+
+    constructor (ws: WebSocket, proc: getDeviceDataFunc){
+        this.ws = ws;
+        this.proc = proc;
+        this.commands = {
+            'get'    : this.getDeviceTags.bind(this),
+            'default': () => {
+                return ErrorMessage('Unknown command');
+            }
+        }
+        this.ws.on('message', this.onMessage.bind(this));
+        this.ws.on('close', this.onClose.bind(this))
+    }
+
+    private async onMessage(message: any) {
+        var result: any;
+        try {
+            const request = validationJSON(message);
+            result = await this.decodeCommand(request);
+        } catch (e) {
+            result = ErrorMessage(e.message || '');
+        }
+        this.ws.send(JSON.stringify(result));
+    }
+
+    private onClose(){
+        console.log('Connection close');
+    }
+
+    private async decodeCommand(cmd: any): Promise<any | IErrorMessage> {
+        const key = this.getCmdName(cmd);
+        return await (this.commands[key] || this.commands['default'])(cmd[key])
+    }
+
+    private getCmdName(cmd: any): string {
+        for (let key in cmd) {
+            return key;
+          }
+        throw new Error ('Invalid request format');
+    }
+
+    private async getDeviceTags (request: any): Promise<any | IErrorMessage> {
+        try {
+            const result = await this.proc(request)
+            return {status:'OK',
+                    time: new Date().toISOString(),
+                    data:result}
+        } catch (e) {
+            return ErrorMessage(e.message || '');
+        }
+    }    
+}
